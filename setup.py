9903856aff9f28d7be320166056bd7d4ed6c3739
Will Sweet <11434284+williebsweet@users.noreply.github.com>
2019-11-06 13:49:22 -0500

    add notes

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(name='singer-target-stitch',
       install_requires=[
           'jsonschema==2.6.0',
           'mock==2.0.0',
+        #   removed because of pip dependency issues
         #   'requests==2.20.0',
         #   'singer-python==5.8.0',
           'psutil==5.3.1',
--- target_stitch/__init__.py
+++ target_stitch/__init__.py
@@ -645,8 +645,8 @@ def main_impl():
         parser.error("config file required if not in dry run mode")
     else:
         config = json.load(args.config)
-        token = config.get('token')
-        # token = os.environ['STS_TOKEN']
+        # token = config.get('token')
+        token = os.environ['STS_TOKEN']
         stitch_url = use_batch_url(config.get('stitch_url', DEFAULT_STITCH_URL))
         turbo_boost_factor = get_turbo_boost_factor(config)
         if not token:
