467241c33be93ec49c582c0966872f2edc687aa2
Will Sweet <11434284+williebsweet@users.noreply.github.com>
2019-11-13 15:19:08 -0500

    Update __init__.py

--- target_stitch/__init__.py
+++ target_stitch/__init__.py
@@ -645,9 +645,7 @@ def main_impl():
         parser.error("config file required if not in dry run mode")
     else:
         config = json.load(args.config)
-        # token = config.get('token')
-        print(os.environ)
-        token = os.environ.get('STITCH_TOKEN')
+        token = config.get('token')
         stitch_url = use_batch_url(config.get('stitch_url', DEFAULT_STITCH_URL))
         turbo_boost_factor = get_turbo_boost_factor(config)
         if not token:
